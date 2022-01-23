a9f6c529042791d6139ad8ac7fd3f176aacf85d5
Elliot McIntyre <35668113+lemcii@users.noreply.github.com>
2022-01-23 00:08:22 +0000

    line numbers and more validation

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,33 @@
       <label class="block mb-2 text-blue-300 font-semibold" for="input">
         Raw List
       </label>
-      <textarea
+      <prism-editor
         id="input"
         name="input"
-        class="bg-gray-800 border border-gray-700 rounded p-4 w-full max-w-xl h-64 focus:outline-none focus:border-blue-500 hover:border-blue-500"
+        class="bg-gray-800 border border-gray-700 rounded px-4 py-3 w-full max-w-xl focus:outline-none focus:border-blue-500 hover:border-blue-500 cursor-text"
         @input="parseRawList($event.target.value)"
+        @click="$event.target.getElementsByTagName('textarea')?.[0]?.focus()"
+        v-model="code"
+        :highlight="errorHighlight"
+        insert-spaces="false"
+        ignore-tab-key
+        line-numbers
       />
     </div>
     <div class="mb-8">
       <div v-if="provisioningListErrors.length">
         <div
-          class="text-red-200 bg-red-900 p-3 rounded border border-red-600 mb-5"
+          class="text-red-200 bg-red-900/75 p-3 rounded border border-red-600 mb-5"
         >
-          <h3 class="font-bold mb-2">Errors</h3>
+          <h3 class="font-bold mb-2 text-white">
+            {{ provisioningListErrors.length }} Error(s) Found
+          </h3>
           <ul>
             <li v-for="error in provisioningListErrors">
-              {{ error }}
+              <span class="font-semibold text-red-100">
+                Line {{ error[0] + 1 }}
+              </span>
+              &ndash; {{ error[1] }}
             </li>
           </ul>
         </div>
@@ -35,7 +46,20 @@
           <h3 class="text-xl mb-2 text-blue-50">
             <span class="font-bold">{{ globalAreaCode }}</span> &ndash;
             {{ globalAreaCodeNames[globalAreaCode] }} &ndash;
-            {{ accounts.length }}
+            <span
+              :class="
+                accounts.length ===
+                accounts.filter(({ provisionedNumber }) => provisionedNumber)
+                  .length
+                  ? 'text-blue-400'
+                  : 'text-red-500'
+              "
+            >
+              {{
+                accounts.filter(({ provisionedNumber }) => provisionedNumber)
+                  .length
+              }}/{{ accounts.length }}
+            </span>
           </h3>
           <div class="space-y-2">
             <div>
@@ -48,15 +72,31 @@
               <textarea
                 :id="`input-${globalAreaCode}`"
                 :name="`input-${globalAreaCode}`"
-                class="bg-gray-800 border border-gray-700 rounded p-4 w-full max-w-xl h-32 focus:outline-none focus:border-blue-500 hover:border-blue-500"
+                :ref="`input-${globalAreaCode}`"
+                class="bg-gray-800 border border-gray-700 rounded px-4 py-3 w-full max-w-xl h-32 focus:outline-none focus:border-blue-500 hover:border-blue-500"
+                :class="
+                  getInputLines(`input-${globalAreaCode}`) > 0 &&
+                  accounts.length !== getInputLines(`input-${globalAreaCode}`)
+                    ? 'text-red-100 border-red-900 hover:border-red-500 focus:border-red-500 bg-red-900/25'
+                    : 'text-white'
+                "
                 @input="
                   updateLines($event.target.value, accounts, globalAreaCode)
                 "
               />
+              <div
+                v-if="
+                  getInputLines(`input-${globalAreaCode}`) &&
+                  accounts.length !== getInputLines(`input-${globalAreaCode}`)
+                "
+                class="mt-1 mb-2 text-red-500"
+              >
+                Invalid line quantity
+              </div>
             </div>
             <div>
               <label
-                class="block text-blue-300 font-semibold"
+                class="block mb-2 text-blue-300 font-semibold"
                 :for="`input-${globalAreaCode}`"
               >
                 Lines
@@ -64,7 +104,7 @@
               <textarea
                 :id="`output-${globalAreaCode}`"
                 :name="`output-${globalAreaCode}`"
-                class="bg-gray-800 border border-gray-700 rounded p-4 w-full max-w-xl h-32 focus:outline-none focus:border-blue-500 hover:border-blue-500"
+                class="bg-gray-800 border border-gray-700 rounded px-4 py-3 w-full max-w-xl h-32 focus:outline-none focus:border-blue-500 cursor-not-allowed"
                 :value="formatLines(accounts)"
                 readonly
               />
@@ -74,8 +114,17 @@
       </div>
     </div>
     <div>
-      <h2 class="text-xl mb-4 font-bold text-blue-50">
-        Number Provising Output
+      <h2 class="text-xl mb-4 text-blue-50">
+        <span class="font-bold">Number Provising Output</span> &ndash;
+        <span
+          :class="
+            provisionedAccountTotal === accountTotal
+              ? 'text-blue-400'
+              : 'text-red-500'
+          "
+        >
+          {{ provisionedAccountTotal }}/{{ accountTotal }}
+        </span>
       </h2>
       <label class="block mb-2 text-blue-300 font-semibold" for="input">
         Final List
@@ -83,36 +132,53 @@
       <textarea
         id="input"
         name="input"
-        class="bg-gray-800 border border-gray-700 rounded p-4 w-full max-w-xl h-64 focus:outline-none focus:border-blue-500 hover:border-blue-500"
+        class="bg-gray-800 border border-gray-700 rounded p-4 w-full max-w-xl h-64 focus:outline-none focus:border-blue-500 cursor-not-allowed"
         ref="finalListInput"
+        readonly
       />
     </div>
   </div>
 </template>
 
 <script setup>
+import { PrismEditor } from "vue-prism-editor";
 import { ref } from "vue";
+import sanitizeHtml from "sanitize-html";
 import postcodeMapping from "./assets/postcodeMapping.json";
 
-/** @typedef {{ accountNumber: string, salesOrder: string, lineNumber: string, provisionedNumber?: string }} Account */
+import "vue-prism-editor/dist/prismeditor.min.css";
+
+/**
+ * @typedef {{ accountNumber: string, salesOrder: string, lineNumber: string, provisionedNumber?: string }} Account
+ */
 
 const date = new Date();
 const today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
+// this is bad, but who cares it's a temporary quick hacky tool
+
+const accountTotal = ref(0);
+const provisionedAccountTotal = ref(0);
+
 /** @type {import("vue").Ref<{ [area: string]: Account[] }>} */
 const accountsByArea = ref({});
 
+/** @type {import("vue").Ref<[number, string][]>} */
 const provisioningListErrors = ref([]);
+
+/** @type {import("vue").Ref<{ [area: string]: string }>} */
 const globalAreaCodeNames = ref({});
+
+/** @type {import("vue").Ref<Element>} */
 const finalListInput = ref();
+
+const regex =
+  /^(\d+)\s+(\d+)\s+(?:Line\s+\d\s+Missing\s+){1,2}([a-z]{1,2}\d{1,2}\s\d[a-z]{2})$/i;
 
 /**
  * @param {string} input
  */
 function parseRawList(text) {
-  const regex =
-    /^(\d+)\s+(\d+)\s+(?:Line\s+\d\s+Missing\s+){1,2}([a-z]{1,2}\d{1,2}\s\d[a-z]{2})$/i;
-
   /** @type {string[]} */
   const lines = text
     .split("\n")
@@ -126,10 +192,14 @@ function parseRawList(text) {
 
   for (const [index, line] of lines.entries()) {
     try {
+      if (line.length === 0) {
+        continue;
+      }
+
       const match = line.match(regex);
 
       if (!match) {
-        throw new Error(`Unable to parse "${line}" on line ${index + 1}`);
+        throw new Error(`Unable to parse "${line}"`);
       }
 
       const [, accountNumber, salesOrder, postCode] = match;
@@ -137,16 +207,12 @@ function parseRawList(text) {
       const postCodeArea = postCode.match(/^([a-z0-9]+)/i);
 
       if (!postCodeArea) {
-        throw new Error(
-          `${accountNumber} (on line ${index + 1}) has invalid postcode`
-        );
+        throw new Error(`Order ${salesOrder} has invalid postcode`);
       }
 
       if (!postcodeMapping[postCodeArea[0]]) {
         throw new Error(
-          `${accountNumber} (on line ${index + 1}) has unknown postcode area: ${
-            postCodeArea[0]
-          }`
+          `Order ${salesOrder} has unknown postcode area: ${postCodeArea[0]}`
         );
       }
 
@@ -158,9 +224,7 @@ function parseRawList(text) {
 
       if (numbersRequired.length === 0 || numbersRequired.length > 2) {
         throw new Error(
-          `${accountNumber} (on line ${
-            index + 1
-          }) wrong number of "line missing" values`
+          `Order ${salesOrder} has incorrect number of "line missing" values`
         );
       }
 
@@ -180,7 +244,10 @@ function parseRawList(text) {
         });
       }
     } catch (error) {
-      provisioningListErrors.value.push(error.message);
+      provisioningListErrors.value = [
+        ...provisioningListErrors.value,
+        [index, error.message],
+      ];
     }
   }
 
@@ -234,8 +301,15 @@ function formatLines(accounts) {
 }
 
 function updateFinalList() {
-  finalListInput.value.value = Object.values(accountsByArea.value)
-    .flat()
+  const accounts = Object.values(accountsByArea.value).flat();
+
+  accountTotal.value = accounts.length;
+
+  provisionedAccountTotal.value = accounts.filter(
+    ({ provisionedNumber }) => provisionedNumber
+  ).length;
+
+  finalListInput.value.value = accounts
     .map(
       ({ accountNumber, salesOrder, provisionedNumber, lineNumber }) =>
         `${accountNumber} ${salesOrder} ${
@@ -243,5 +317,42 @@ function updateFinalList() {
         } ${lineNumber} NEW`
     )
     .join("\n");
+}
+
+/**
+ * @param {string} code
+ */
+function errorHighlight(code) {
+  const lines = code.split("\n");
+
+  console.log(lines, [
+    ...provisioningListErrors.value.map((value) => [...value]),
+  ]);
+
+  return lines
+    .map((line, index) =>
+      line.trim().length
+        ? `<span class="${
+            provisioningListErrors.value.find(
+              ([errorIndex]) => index === errorIndex
+            )
+              ? "underline decoration-wavy decoration-red-500 bg-red-900/50"
+              : "white"
+          }">${sanitizeHtml(line)}</span>`
+        : line
+    )
+    .join("\n");
+}
+
+/**
+ * @param {string} id
+ */
+function getInputLines(id) {
+  return (
+    document
+      .getElementById(id)
+      ?.value?.split("\n")
+      .filter((line) => line.trim().length).length || 0
+  );
 }
 </script>
